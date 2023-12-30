d09f49a14c760484203dd04cfec1eba6011537ab
fraigneau <fraigneau86@yahoo.fr>
2023-12-30 14:53:23 +0100

    add delay for encodeur

--- keyboards/rotary/config.h
+++ keyboards/rotary/config.h
@@ -4,8 +4,8 @@
 #pragma once
 
 #define ENCODERS_PAD_A { F4, F7 }
-#define ENCODERS_PAD_B { F5, F4 }
-#define ENCODER_RESOLUTIONS { 4 }
-
+#define ENCODERS_PAD_B { F5, F6 }
+#define ENCODER_RESOLUTIONS { 4, 2 }
+#define ENCODER_MAP_KEY_DELAY 10
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 2
